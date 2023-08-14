4953e7211e714fde12e67aa133f482e47f158bfa
muna-amir <maimuamir@gmail.com>
2023-08-14 13:29:35 -0700

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Script that prints C is fun,Python is cool and JavaScript is amazing
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
